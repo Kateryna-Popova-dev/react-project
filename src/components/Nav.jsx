c12982671d92b2c0b64841e3cef63d50a7ae4b0d
Kate Popova <93034028+katenysh@users.noreply.github.com>
2022-10-24 21:35:02 +0300

    git add

---
+++ src/components/Nav.jsx
@@ -0,0 +1,27 @@
+const Nav = () => {
+    return (
+        <ul>
+            <li>
+                <a href="./" className="nav_link">
+                    Link
+                </a>
+            </li>
+            <li>
+                <a href="./" className="nav_link">
+                    Link
+                </a>
+            </li>
+            <li>
+                <a href="./" className="nav_link">
+                    Link
+                </a>
+            </li>
+            <li>
+                <a href="./" className="nav_link">
+                    Link
+                </a>
+            </li>
+        </ul>
+    )
+}
+export {Nav}
