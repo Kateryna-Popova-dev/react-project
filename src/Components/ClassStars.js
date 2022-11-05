08abaf176abb5082b7361f5729b1f15c59754e04
Kate Popova <93034028+katenysh@users.noreply.github.com>
2022-11-06 00:16:36 +0200

    Changed ClassStars component

--- src/Components/ClassStars.js
+++ src/Components/ClassStars.js
@@ -4,27 +4,26 @@ import svg from "../image/star-icon.svg";
 class ClassStars extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {points: props.points}
+        this.state = {
+            points: props.points,
+            dynamicPoints: 0,
+        }
     }
 
     count = [1, 2, 3, 4, 5];
-    toggleClass = (event) => {
-        this.count.forEach(i => {
-            if (event.target.closest(`#a${i}`)) {
-
-                this.setState({points: i})
-            }
-        })
-    }
+     getPointFromStar = (event) => this.count.find(i => event.target.closest(`#a${i}`));
 
     render() {
-        return (
-            <div onPointerOver={this.toggleClass} onClick={this.toggleClass} className="container">
-                {this.count.map(i =>
-                    <img id={`a${i}`} className={i <= this.state.points ? 'isActive' : null} src={svg}/>
-                )}
-            </div>
-        );
+        return <div
+            className="container"
+            onPointerOut={() => this.setState({dynamicPoints:this.state.points})}
+            onClick={(event) => this.setState({points: this.getPointFromStar(event)})}
+            onPointerOver={(event) => this.setState({dynamicPoints: this.getPointFromStar(event)})}
+        >
+            {this.count.map(i =>
+                <img id={`a${i}`} className={i <= this.state.dynamicPoints ? 'isActive' : null} src={svg} key={i}/>
+            )}
+        </div>;
     }
 }
 
